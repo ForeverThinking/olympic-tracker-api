6ee0932af1ca31ba8a5e340ab436459526b9dffc
ForeverThinking <35239092+ForeverThinking@users.noreply.github.com>
2024-08-05 22:42:51 +0100

    add env support

--- src/main.go
+++ src/main.go
@@ -3,11 +3,22 @@ package main
 import (
     "fmt"
     "net/http"
+    "os"
+    "log"
+
+    "github.com/joho/godotenv"
 )
 
 func main() {
+    err := godotenv.Load("../.env")
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    test := os.Getenv("TEST")
+
     http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        fmt.Fprint(w, "Testing")
+        fmt.Fprint(w, test)
     })
 
     http.ListenAndServe(":8080", nil)
